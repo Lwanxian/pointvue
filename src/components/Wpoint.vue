8aa08c3158fa2d144597ef4b216b8992f5730c70
Lwanxian <L_wanxian@163.com>
2021-04-21 11:49:22 +0800

    camera position

--- src/components/Wpoint.vue
+++ src/components/Wpoint.vue
@@ -4,26 +4,27 @@
 
 <script>
 import * as THREE from "three/build/three.module";
+import Stats from "three/examples/jsm/libs/stats.module";
 import { GUI } from "three/examples/jsm/libs/dat.gui.module";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import Stats from "three/examples/jsm/libs/stats.module";
+
 export default {
   data() {
     return {
       camera: null,
       scene: null,
+      renderer: null,
+      stats: null,
+      mesh: null,
+      amount: null,
+      raycaster: null,
+      mouse: null,
       light1: null,
       light2: null,
       geometry: null,
       material: null,
-      renderer: null,
-      mesh: null,
-      amount: null,
       axisHelper: null,
       controls: null,
-      stats: null,
-      mouse: null,
-      raycaster: null,
     };
   },
   methods: {
@@ -37,25 +38,28 @@ export default {
         0.1,
         100
       );
+      this.amount = parseInt(window.location.search.substr(1)) ||10 ;
+      let count = Math.pow(this.amount, 3);
+      this.camera.position.set(this.amount, this.amount, this.amount);
+      // 灯光
       this.light1 = new THREE.HemisphereLight(0xffffff, 0x111188);
       this.light1.position.set(-1, 1.5, 1);
 
-      this.light2 = new THREE.HemisphereLight(0xbb2233,  0x080820, 0.5);
+      this.light2 = new THREE.HemisphereLight(0xbb2233, 0x080820, 0.5);
       this.light2.position.set(-1, -1.5, -1);
       this.scene.add(this.light1, this.light2);
       // 几何体创建,二十面以上的多面体，当第二个参数大于1时为球体
       this.geometry = new THREE.IcosahedronGeometry(0.1, 4);
       // 材质对象
       this.material = new THREE.MeshPhongMaterial();
-      let count = 1000;
-      this.mesh = new THREE.InstancedMesh(this.geometry, this.material, count);
       let i = 0;
 
-      this.amount = parseInt(window.location.search.substr(1)) || 10;
+      this.mesh = new THREE.InstancedMesh(this.geometry, this.material, count);
+
       let offset = (this.amount - 1) / 2;
       let matrix = new THREE.Matrix4();
       let color = new THREE.Color();
- 
+
       for (let x = 0; x < this.amount; x++) {
         for (let y = 0; y < this.amount; y++) {
           for (let z = 0; z < this.amount; z++) {
@@ -65,7 +69,7 @@ export default {
             i++;
           }
         }
-      }
+      }     
       this.scene.add(this.mesh);
 
       let gui = new GUI();
@@ -77,29 +81,27 @@ export default {
       this.$refs.container.appendChild(this.renderer.domElement);
       this.stats = new Stats();
       this.$refs.container.appendChild(this.stats.dom);
-      // 辅助坐标系
-      this.axisHelper = new THREE.AxesHelper(200);
-      this.scene.add(this.axisHelper);
 
       window.addEventListener("resize", this.onWindowResize);
       this.$refs.container.addEventListener("mousemove", this.onMouseMove);
-      
-      this.controls = new OrbitControls(this.camera, this.renderer.domElement); //创建控件对象（鼠标控制）
-      this.controls.update();
-
-
     },
     onWindowResize: function () {
       this.camera.aspect = window.innerWidth / window.innerHeight;
       this.camera.updateProjectionMatrix();
       this.renderer.setSize(window.innerWidth, window.innerHeight);
-    }, 
+    },
 
     onMouseMove: function (event) {
       event.preventDefault();
       this.mouse = new THREE.Vector2(1, 1);
       this.mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
       this.mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+    },
+    initControls: function () {
+      this.controls = new OrbitControls(this.camera, this.renderer.domElement); //创建控件对象（鼠标控制）
+      this.controls.enableDamping = true; // 使动画循环使用时阻尼或自转 意思是否有惯性
+      this.controls.enableZoom = true; //是否可以缩放
+      this.controls.enablePan = true; //是否开启右键拖拽
     },
 
     render() {
@@ -114,14 +116,17 @@ export default {
         this.mesh.instanceColor.needsUpdate = true;
       }
       this.renderer.render(this.scene, this.camera); //执行渲染操作
-      requestAnimationFrame(this.render);
-      this.stats.update();
     },
-    
+    animate() {
+      requestAnimationFrame(this.animate);
+      this.stats.update();
+      this.render();
+    },
   },
   mounted() {
     this.init();
-    this.render();
+    this.animate();
+    this.initControls();
   },
 };
 </script>
